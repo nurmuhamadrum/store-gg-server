f837dc62dc04f5119729367913c0e13a211c4e7b
nurmuhamadrum <nurmuhamadrum@gmail.com>
2021-12-18 19:50:53 +0700

    handle success and error message

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override')
-
+const session = require('express-session')
+const flash = require('connect-flash');
 const dashboardRouter = require('./app/dashboard/router');
 const categoryRouter = require('./app/category/router');
 
@@ -19,6 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}))
 app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte/')))
 app.use('/', dashboardRouter);
 app.use('/category', categoryRouter);
--- app/category/controller.js
+++ app/category/controller.js
@@ -3,12 +3,20 @@ const Category = require('./model')
 module.exports = {
     index: async (req, res) => {
         try {
+            const alertMessage = req.flash('alertMessage')
+            const alertStatus = req.flash('alertStatus')
+            const alert = { message: alertMessage, status: alertStatus }
             const category = await Category.find();
+
             res.render('admin/category/view_category', {
-                category
+                category,
+                alert
             })
+            
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');
         }
     },
 
@@ -17,63 +25,81 @@ module.exports = {
             await res.render('admin/category/create');
 
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');
         }
     },
 
     actionCreate: async (req, res) => {
         try {
             const { name } = req.body;
-
             let category = await Category({ name });
             await category.save();
 
+            req.flash('alertMessage', 'Successfully added category');
+            req.flash('alertStatus', 'success')
             res.redirect('/category');
+
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');;
         }
     },
 
     viewEdit: async (req, res) => {
         try {
             const { id } = req.params;
-
             const category = await Category.findOne({ _id: id });
 
             res.render('admin/category/edit', {
                 category
             });
+
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');
         }
     },
 
-    actionEdit: async(req, res) => {
+    actionEdit: async (req, res) => {
         try {
             const { id } = req.params;
             const { name } = req.body
 
-            await Category.findByIdAndUpdate({ 
-                _id: id 
+            await Category.findByIdAndUpdate({
+                _id: id
             }, { name });
 
+            req.flash('alertMessage', 'Successfully edit category');
+            req.flash('alertStatus', 'success')
             res.redirect('/category');
+
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');
         }
     },
 
-    actionDelete: async(req, res) => {
+    actionDelete: async (req, res) => {
         try {
             const { id } = req.params;
 
-            await Category.findByIdAndRemove({ 
-                _id: id 
+            await Category.findByIdAndRemove({
+                _id: id
             });
-            
+
+            req.flash('alertMessage', 'Successfully delete category');
+            req.flash('alertStatus', 'success')
             res.redirect('/category');
+
         } catch (error) {
-            console.log(error);
+            req.flash('alertMerssage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/category');
         }
     }
 }
